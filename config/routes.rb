c197ca09e0fadec2d53f0189152c5f3ef6c79cc7
Ranika Williams <rnkprice07@gmail.com>
2019-11-03 18:47:28 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :songs
-  get '/', to: 'songs#index'
+  get '/', to: 'songs#index' as: songs_path
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
